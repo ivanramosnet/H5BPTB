a08aa4c4bff388e634bfdfaa49ba6be0fa1f6b66
Iván Ramos Jiménez <info@ivan.ramos.name>
2012-02-21 13:44:10 +0000

    Added Twitter Bootstrap 2 support. HTML5 Boilerplate + Twitter BootStrap. Based on Initializr.

--- component.php
+++ component.php
@@ -11,15 +11,14 @@
 defined('_JEXEC') or die;
 ?>
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<jdoc:include type="head" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/style.css">
+	 <link rel="stylesheet/less" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/less/style.less">
+	<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/libs/less-1.2.1.min.js"></script>
 </head>
 <body class="contentpane">
 	<jdoc:include type="message" />
--- error.php
+++ error.php
@@ -15,11 +15,9 @@ if (!isset($this->error)) {
 }
 ?>
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
