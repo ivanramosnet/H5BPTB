4a29959b307d64ce34a6a81f9fda83f74474e1e0
Iván Ramos Jiménez <info@ivan.ramos.name>
2012-11-28 13:28:43 +0000

    Updated to jQuery 1.8.3

--- index.php
+++ index.php
@@ -142,8 +142,8 @@ $app = JFactory::getApplication();
 	
 	<jdoc:include type="modules" name="debug" />
 	
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/libs/jquery-1.8.0.min.js"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/libs/jquery-1.8.3.min.js"><\/script>')</script>
 	
 	<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/js/libs/bootstrap/bootstrap.min.js"></script>
 	
